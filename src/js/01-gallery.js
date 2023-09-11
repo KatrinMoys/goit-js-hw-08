0d7bc7e4d3cf439ff56294616600df0456dc9c55
KatrinMoys <112271327+KatrinMoys@users.noreply.github.com>
2023-09-12 00:06:53 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import '../css/common.css';
 import '../css/01-gallery.css';
